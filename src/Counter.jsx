8a1111264fa68d76fc05b1a1cf6fa9b9a30cb99c
Nirav Soni <soninirav910@gmail.com>
2023-02-10 19:12:19 +0530

    Added +1 and -1 buttons

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -2,6 +2,11 @@ import React from "react";
 
 export default class Counter extends React.Component{
     render(){
-        return(<div>Counter</div>);
+        return(
+        <div className="row text-white">
+            <h1>Counter: </h1>
+            <button style={{width: "200px"}}>+1</button>
+            <button style={{width: "200px"}}>-1</button>
+        </div>);
     }
 }
