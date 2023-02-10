2d1e92c92309c246178d8b347f8199a81be6d759
Nirav Soni <soninirav910@gmail.com>
2023-02-10 19:46:37 +0530

    UI changes

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import attack from "./images/attack.png";
+import defend from "./images/defend.png";
 
 export default class Counter extends React.Component{
     constructor(props){
@@ -28,10 +30,33 @@ export default class Counter extends React.Component{
 
     render(){
         return(
-        <div className="row text-white">
-            <h1>Counter: {this.state.count} </h1>
-            <button onClick={this.handleAttack} style={{width: "200px"}}>+1</button>
-            <button onClick={this.handleDefence} style={{width: "200px"}}>-1</button>
+        <div className="row text-white text-center">
+            <h1>Gane Score: {this.state.count} </h1>
+            <p>You win at +10 points and lose at -10 points!</p>
+            <p>Last Play: </p>
+            <p>Game Status: </p>
+            <div className="col-6 col-md-3 offset-md-3">
+                <img
+                style={{
+                    width: "100%",
+                    cursor: "pointer",
+                    border: "1px solid green",
+                }}
+                className="p-4 rounded" src={attack} onClick={this.handleAttack}></img>
+            </div>
+            <div className="col-6 col-md-3 offset-md-3">
+                <img
+                style={{
+                    width: "100%",
+                    cursor: "pointer",
+                    border: "1px solid red",
+                }}
+                className="p-4 rounded" src={defend} onClick={this.handleDefence}></img>
+            </div>
+            <div className="col-12 col-md-4 offset-md-4">
+                <button className="btn btn-secondary w-100 mt-2">Random Play</button>
+                <button className="btn btn-warning w-100 mt-2">Reset</button>
+            </div>
         </div>);
     }
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ export default function Header(){
     return(
         <div className="pt-3 pl-2 py-2" style={{borderBotton:"1px solid #777"}}>
             <img src={logo} alt="" style={{height: "35px", verticalAlign: "top"}} />
-            <span className="h2 pt-4 m-2 text-white-50">React Course - CountOpedia</span>
+            <span className="h2 pt-4 m-2 text-white-50">CountOpedia</span>
         </div>
     )
 }
